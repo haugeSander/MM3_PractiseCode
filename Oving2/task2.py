93f5f11a3f8cc48d37268fd3a628f7ef1d7bc59a
Sander Hauge <sandeth@stud.ntnu.no>
2023-08-31 14:45:16 +0200

    Fix: oving2, task2 now works. Add: task9, not yet functional

--- Oving2/task2.py
+++ Oving2/task2.py
@@ -2,32 +2,28 @@ import numpy as np
 
 
 def fikspunkt_solve(f, x0, tol):
-    x = x0
+    iteration = 0
 
-    for i in range(8):
-        x = f(x)
-        print(x)
-
-        if abs(x - x0) < tol:
+    while iteration < 12:
+        x = f(x0)
+        if abs(x - x0) <= tol:
             return x
         x0 = x
+        iteration += 1
 
     return x
 
 
-f1 = lambda x: x ** 3 - 2 * x - 2  # Hvilken funksjon skal inn i fikspunkt_solve? (lign. 1)
-f2 = lambda x: np.exp(x) + x - 7  # Hvilken funksjon skal inn i fikspunkt_solve? (lign. 2)
-f3 = lambda x: np.exp(x) + np.sin(x) - 4  # Hvilken funksjon skal inn i fikspunkt_solve? (lign. 3)
+f1 = lambda x: np.cbrt(2*x+2)  # Hvilken funksjon skal inn i fikspunkt_solve? (lign. 1)
+f2 = lambda x: np.log(7-x)  # Hvilken funksjon skal inn i fikspunkt_solve? (lign. 2)
+f3 = lambda x: np.log(4 - np.sin(x))  # Hvilken funksjon skal inn i fikspunkt_solve? (lign. 3)
 funcs = [f1, f2, f3]
-maks_feil = 1 * 10 ** (-6)  # ????
-expected_sols = [1.5, 1.5, 1]
+maks_feil = 1 * 10 ** (-8)
 sols = []
-iteration = 0
 
 for f in funcs:
     x_sol = fikspunkt_solve(f, 1, maks_feil)
     sols.append(x_sol)
-    iteration += 1
 
 print(f"Løsning til første ligning x = {sols[0]}")
 print(f"Løsning til andre ligning x = {sols[1]}")
---
+++ Oving2/task9.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def naive_gauss(A, b):
+    n, m = np.shape(A)
+    S = np.zeros((n, n + 1))
+    S[:, 0:n] = A
+    S[:, -1] = b
+    for j in range(n - 1):
+        for i in range(j + 1, n):
+            mult = S[i, j] / S[j, j]
+            S[i, j] = 0.0
+            for k in range(i, m):
+                S[i, k] = S[i, k]-mult*S[j,k]
+
+    return S[:, 0:n], S[:, -1]
+
+# Forslag til fremgangsmåte til definere A, b
+# -Viser noen nyttige np-funksjoner
+A = np.array([1,2,-1,0,3,1,2,-1,1])
+A = A.reshape((3, 3))
+b = np.array([2,4,2]).T
+Ar, br = naive_gauss(A, b)
+print(Ar)
+print(br)
